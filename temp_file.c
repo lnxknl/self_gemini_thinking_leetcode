66da8253d1960f5772e7777c0e9da1b5467300cd
lnxknl4 <jjmutong@gmail.com>
2024-12-31 23:44:12 +0800

    Imagine a room represented by a set of locations. Each location can contain multiple items. You are given a list of items you need to retrieve before going to sleep. Retrieving an item from a location takes a certain amount of time. Moving between locations also takes time. Your goal is to find the most efficient order to retrieve all the required items, minimizing the total time spent.
    **Formal Definition:**
    
    * You are given:
        * `locations`: An array of strings representing the names of different locations in the room (e.g., "Shelf", "Drawer", "Table").
        * `items_at_location`: A data structure (e.g., a hash table or a 2D array) mapping each location to the list of items it contains.
        * `required_items`: An array of strings representing the items you need to retrieve.
        * `retrieval_time`: An integer representing the time taken to retrieve one item from a location.
        * `movement_time`: An integer representing the time taken to move between any two distinct locations.
    
    * Your task is to find the minimum time required to retrieve all `required_items`. You start at an arbitrary location (we can assume the initial location doesn't add to the cost since the focus is on relative movement and retrieval).
    
    **Data Structures:**
    
    * **Hash Table (or Hash Map):**  `items_at_location` can be efficiently represented using a hash table where the key is the location name (string) and the value is a linked list (or an array) of items present at that location. This allows for quick lookups of items within a location.
    
    Signed-off-by: lnxknl4 <jjmutong@gmail.com>

--- temp_file.c
+++ temp_file.c
@@ -1,124 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 
-typedef struct {
-    int start_time;
-    int end_time;
-    int disturbance_level;
-} Activity;
+#define MAX_LOCATIONS 10
+#define MAX_ITEMS 20
+#define MAX_ITEM_NAME_LENGTH 50
 
+// Structure to represent a location and its items
 typedef struct {
-    int time;
-    int type; // 0: sleep_start, 1: sleep_end, 2: start_activity, 3: end_activity
-    int disturbance;
-    int activity_index; // For identifying the activity ending
-} Event;
+    char name[MAX_ITEM_NAME_LENGTH];
+    char items[MAX_ITEMS][MAX_ITEM_NAME_LENGTH];
+    int num_items;
+} Location;
 
-int compareEvents(const void *a, const void *b) {
-    Event *eventA = (Event *)a;
-    Event *eventB = (Event *)b;
-    if (eventA->time != eventB->time) {
-        return eventA->time - eventB->time;
+// Function to find the location of an item
+int find_item_location(Location locations[], int num_locations, const char *item_name, int *location_index) {
+    for (int i = 0; i < num_locations; i++) {
+        for (int j = 0; j < locations[i].num_items; j++) {
+            if (strcmp(locations[i].items[j], item_name) == 0) {
+                *location_index = i;
+                return 1; // Found
+            }
+        }
     }
-    return eventA->type - eventB->type; // Prioritize sleep start, then activity start, then activity end, then sleep end
+    return 0; // Not found
 }
 
-int calculateLostSleep(Activity *activities, int num_activities, int sleep_start, int sleep_end, int disturbance_threshold) {
-    int sleep_capacity = sleep_end - sleep_start;
-    if (sleep_capacity <= 0) return 0;
-
-    int num_events = num_activities * 2 + 2;
-    Event events[num_events];
-    int event_index = 0;
-
-    events[event_index++] = (Event){sleep_start, 0, 0, -1};
-    events[event_index++] = (Event){sleep_end, 1, 0, -1};
-
-    for (int i = 0; i < num_activities; i++) {
-        events[event_index++] = (Event){activities[i].start_time, 2, activities[i].disturbance_level, i};
-        events[event_index++] = (Event){activities[i].end_time, 3, activities[i].disturbance_level, i};
+int solve_item_retrieval(Location locations[], int num_locations, char required_items[][MAX_ITEM_NAME_LENGTH], int num_required_items, int retrieval_time, int movement_time) {
+    int total_time = 0;
+    int current_location_index = -1; // Start at no specific location
+    bool retrieved[num_required_items];
+    for (int i = 0; i < num_required_items; i++) {
+        retrieved[i] = false;
     }
 
-    qsort(events, num_events, sizeof(Event), compareEvents);
+    int remaining_items = num_required_items;
 
-    int accumulated_disturbance = 0;
-    int good_sleep_obtained = 0;
-    bool is_sleeping = false;
-    bool disrupted = false;
-    int last_event_time = 0;
+    while (remaining_items > 0) {
+        int best_next_item_index = -1;
+        int min_time_to_retrieve = -1;
 
-    for (int i = 0; i < num_events; i++) {
-        int current_time = events[i].time;
-        int time_elapsed = current_time - last_event_time;
+        for (int i = 0; i < num_required_items; i++) {
+            if (!retrieved[i]) {
+                int target_location_index;
+                if (find_item_location(locations, num_locations, required_items[i], &target_location_index)) {
+                    int time_to_reach = (current_location_index != -1 && current_location_index != target_location_index) ? movement_time : 0;
+                    int current_time_to_retrieve = time_to_reach + retrieval_time;
 
-        if (is_sleeping && !disrupted && time_elapsed > 0) {
-            int disturbance_this_interval = accumulated_disturbance * time_elapsed;
-            if (accumulated_disturbance > 0) {
-                 if (accumulated_disturbance > 0 && accumulated_disturbance > disturbance_threshold) {
-                    disrupted = true;
-                } else {
-                    good_sleep_obtained += time_elapsed;
+                    if (best_next_item_index == -1 || current_time_to_retrieve < min_time_to_retrieve) {
+                        min_time_to_retrieve = current_time_to_retrieve;
+                        best_next_item_index = i;
+                        // Update the target location for the best item
+                        current_location_index = target_location_index;
+                    }
                 }
-            } else {
-                good_sleep_obtained += time_elapsed;
             }
         }
 
-        switch (events[i].type) {
-            case 0: // sleep_start
-                is_sleeping = true;
-                break;
-            case 1: // sleep_end
-                is_sleeping = false;
-                break;
-            case 2: // start_activity
-                if (current_time >= sleep_start && current_time < sleep_end) {
-                    accumulated_disturbance += events[i].disturbance;
-                }
-                break;
-            case 3: // end_activity
-                if (current_time > sleep_start && current_time <= sleep_end) {
-                    accumulated_disturbance -= events[i].disturbance;
-                    if (accumulated_disturbance < 0) accumulated_disturbance = 0; // Avoid negative disturbance
-                }
-                break;
+        if (best_next_item_index != -1) {
+            total_time += min_time_to_retrieve;
+            retrieved[best_next_item_index] = true;
+            remaining_items--;
+        } else {
+            // Should not happen if all required items exist
+            break;
         }
-        last_event_time = current_time;
     }
 
-    if (disrupted) {
-        return sleep_capacity;
-    } else {
-        return sleep_capacity - good_sleep_obtained;
-    }
+    return total_time;
 }
 
 int main() {
-    Activity activities[] = {
-        {10, 20, 2},
-        {25, 30, 5},
-        {40, 50, 3}
+    // Example Input (based on the image hints)
+    Location locations[MAX_LOCATIONS] = {
+        {"Shelf", {"Book1", "Decoration", "Hat"}, 3},
+        {"Drawer", {"Socks", "Keys"}, 2},
+        {"Door Hook", {"Jacket"}, 1}
     };
-    int num_activities = sizeof(activities) / sizeof(activities[0]);
-    int sleep_start = 15;
-    int sleep_end = 45;
-    int disturbance_threshold = 8;
+    int num_locations = 3;
 
-    int lost_sleep = calculateLostSleep(activities, num_activities, sleep_start, sleep_end, disturbance_threshold);
-    printf("Lost Sleep: %d\n", lost_sleep); // Expected output will vary based on precise interpretation of disturbance
+    char required_items[MAX_ITEMS][MAX_ITEM_NAME_LENGTH] = {"Hat", "Keys", "Jacket"};
+    int num_required_items = 3;
 
-    // Test case 2: No overlap
-    Activity activities2[] = {{5, 8, 5}};
-    int num_activities2 = sizeof(activities2) / sizeof(activities2[0]);
-    int lost_sleep2 = calculateLostSleep(activities2, num_activities2, sleep_start, sleep_end, disturbance_threshold);
-    printf("Lost Sleep 2: %d\n", lost_sleep2); // Expected: 0
+    int retrieval_time = 2;
+    int movement_time = 3;
 
-    // Test case 3: High disturbance, threshold crossed
-    Activity activities3[] = {{20, 30, 10}};
-    int num_activities3 = sizeof(activities3) / sizeof(activities3[0]);
-    int lost_sleep3 = calculateLostSleep(activities3, num_activities3, sleep_start, sleep_end, disturbance_threshold);
-    printf("Lost Sleep 3: %d\n", lost_sleep3); // Expected: 30
+    int min_time = solve_item_retrieval(locations, num_locations, required_items, num_required_items, retrieval_time, movement_time);
+
+    printf("Minimum time to retrieve all items: %d\n", min_time);
 
     return 0;
 }
